cfe4059dc1d71191e09ff925908d3c23599d8582
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-10-31 23:56:36 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -28,43 +28,48 @@ function getRandomArbitrary(min, max) {
 }
 
 dragElement(".desktopApp");
-const apps = ['terminal', 'copilasii', 'pcuvant', 'gta6'];
+const apps = ['terminal', 'copilasii', 'pcuvant', 'gta6', 'bahoiImage', 'ppe',
+              'GChelutzu'];
 for( i = 0; i < apps.length; i++){
   let j = i; 
   fetch("static/js/local.json").then(response => response.json()).then(data => {
-    const apps = ['terminal', 'copilasii', 'pcuvant', 'gta6']; 
+    const apps = ['terminal', 'copilasii', 'pcuvant', 'gta6', 'bahoiImage', 'ppe',
+                  'GChelutzu']; 
       //document.querySelector('.' + apps[j] + 'Popup').style.display = 'none';
       document.querySelector('.' + apps[j] + 'Desktop').ondblclick = function()
         {
          
           var element = document.createElement('div');
           element.classList.add('desktopApp', `${apps[j]}Popup`);
-          console.log(element);
+          //console.log(element);
 
           document.querySelector('.popupLocation').appendChild(element)
           element.style.display = 'block';
           getJSONelement = eval(`data.${apps[j]}`);
           
           element.style.border = '3px solid #D4D3D2';
-          element.style.borderStyle = 'outset';
+          element.style.borderStyle = 'outset'; 
           element.style.width = (getJSONelement.width) + 'rem'; 
-          element.style.height = (getJSONelement.height) + 'rem'; 
+          element.style.height = (getJSONelement.height ) + 'rem'; 
+
+          console.log(getJSONelement.height); console.log(getJSONelement.height - 1)
 
           element.style.backgroundColor = getJSONelement.backgroundColor;
           element.style.top = (getRandomArbitrary(window.innerHeight * 0.25, window.innerHeight * 0.3)) + 'px';
           element.style.left = (getRandomArbitrary(window.innerWidth * 0.25, window.innerWidth * 0.75)) + 'px';
-
+          var icon = getJSONelement.icon; var title = getJSONelement.title;
           //calling the drag function after appending child in the popupLocation div to be movable
           dragElement(".desktopApp");
           element.innerHTML = 
-            `<div 
+            `<div style = 'padding-bottom : .2rem; background-color : #D4D3D2;'>
+              <div 
               style = 'gap : .33rem; padding-left : .33rem; padding : .2rem; height : 1rem; background : linear-gradient(45deg, #7FA9C4, #2D536B)'
-              class = 'display-flex-align-items-center'
-              >
-                <img src = 'assets/${apps[j]}.png' style = 'height : 125%;'> ${apps[j]}
+              class = 'display-flex-align-items-center'>
+                <img src = 'assets/icons/${icon}' style = 'height : 100%;'> <span style = 'width : 75%; display : flex; align-items : start;'>${title}</span>
                 <div class = 'w-100 display-flex-align-items-center' style = 'justify-content : right; padding-right : .33rem'>
                   <button class = '${apps[j]}Close' onclick = "document.querySelector('.popupLocation').removeChild(document.querySelector('.${apps[j]}Popup'));"> x </button>
                 </div>
+              </div>
             </div>
             ${getJSONelement.content}`;
         };
