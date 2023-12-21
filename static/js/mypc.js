fc67adfb0d8e12eb20e3a17ddba7a1e5e0044978
Z4que <114612941+Zaque-69@users.noreply.github.com>
2023-12-22 01:10:10 +0200

    Update mypc.js

--- static/js/mypc.js
+++ static/js/mypc.js
@@ -19,20 +19,27 @@ var count = 0;
 
 
 document.getElementById('PC').ondblclick = function(){
-    const animations = ['fadeIn', 'rotation', 'rotation2', 'translateX'];
 
     //declaration of some repetititve variables
     var input = document.getElementById('mypcInput')
 
+    //Everytime when it will be an Enter tappen when you are writing in input, 
+    //it will display the apps from that dir
+    document.getElementById('mypcInput').addEventListener('keypress', function(event){
+    if( event.key === 'Enter' ) sortElements(input.value.replaceAll('/', '_'));
+    })
 
+    //aniamtions when displaying the window
+    const animations = ['fadeIn', 'rotation', 'rotation2', 'translateX'];
     document.querySelector('.pcAppsWindow').style.animation = `${animations[Math.floor(Math.random()*animations.length)]}  .5s`;
-
     document.querySelector('.pcAppsWindow').style.display = 'block';
    
     /*
-      the filterAppsMyPc function filter everytime the apps by displaying 
-      the apps with the value from -input- variable ~ document.getElementById('mypcInput')
-      
+      the sortElements function filter everytime the apps by displaying 
+      the apps with the value from -input- variable ~ document.getElementById('mypcInput').
+      You will notice that this function is used only in this form : sortElements(input.value.replaceAll('/', '_'))
+      that is because JSON don't allow '/' character, so the dirs were written with '_' and replaced
+
     */
     function sortElements(displayBlock){
       document.querySelectorAll('.mypcAppsWindowIcon ').forEach((e) => { e.style.display = 'none'; } )
@@ -54,22 +61,44 @@ document.getElementById('PC').ondblclick = function(){
     document.querySelector('.chelutzu_assets').ondblclick = function(){sortElements('ProgramFiles_chelutzu_assets')}
 
     //images
-    /**
-     
-    document.querySelector('.ciordeala').onclick = function(){
-      console.log('ceva')
-      document.querySelector('.ciordeala').setAttribute('href', 'chelutzu/assets/ciordeala.jpg')
-    }
-     */
-
+   
     //Here are the buttons and the functions they execute from MyPc 
-    document.querySelector('.goButton').addEventListener('click', (event) => sortElements(input.value.replaceAll('/', '_')));
-    document.querySelector('.homeButton').addEventListener('click', (event) => {
-      input.value = 'ProgramFiles'; 
-      //get the value from input and filter the apps by displaying 
-      sortElements(input.value.replaceAll('/', '_'));
-    });
-    document.getElementById('mypcInput').addEventListener('keypress', function(event){
-        if( event.key === 'Enter' ) sortElements(input.value.replaceAll('/', '_'));
+    //{
+      document.querySelector('.goButton').addEventListener('click', () => sortElements(input.value.replaceAll('/', '_')));
+      document.querySelector('.homeButton').addEventListener('click', () => {
+        input.value = 'ProgramFiles'; 
+        //get the value from input and filter the apps by displaying 
+        sortElements(input.value.replaceAll('/', '_'));
+      });
+      document.querySelector('.backButton').addEventListener('click', () => {
+
+        /*
+          the main dir is ProgramFiles, so if the value of input is equal 
+          to ProgramFiles it will return nothing, else will delete the last 
+          '/' and app/element from the dir
+         */
+
+        if(input.value == 'ProgramFiles') return
+
+        else{
+          var listOfChars = input.value.split("")
+          var lipsa = ''; var count = 0; var count2 = 0;
+  
+          for(let i = 0; i < listOfChars.length; i++)
+            if (listOfChars[i] == '/' ) 
+              count++;
+  
+          for(let i = 0; i < listOfChars.length; i++){
+              lipsa += listOfChars[i]; 
+              if( listOfChars[i] == '/') count2++;
+              if (count2 == count ) break;
+            }
+        }
+       
+        console.log(lipsa, lipsa.slice(0, -1))
+      input.value = lipsa.slice(0, -1); sortElements(input.value.replaceAll('/', '_'));
+    //}
+
     })
+
 };
